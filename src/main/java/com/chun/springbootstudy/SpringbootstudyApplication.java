c6fcf8efcdee903f4d928e556f4e81f8d0c59bd9
chunxhwan <chunxhwan@126.com>
2022-04-21 14:37:39 +0800

    ## 日志的使用(slf4j) 输出启动成功 和 服务地址

--- src/main/java/com/chun/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/chun/springbootstudy/SpringbootstudyApplication.java
@@ -1,19 +1,26 @@
 package com.chun.springbootstudy;
 
 import com.chun.springbootstudy.domain.Pet;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
 
 @SpringBootApplication
 public class SpringbootstudyApplication {
 
+    private static final Logger LOG = LoggerFactory.getLogger(SpringbootstudyApplication.class);
+
     public static void main(String[] args) {
         //        final ConfigurableApplicationContext run = SpringApplication.run(SpringbootstudyApplication.class, args);
 
-        final SpringApplication application = new SpringApplication();
-        application.setAdditionalProfiles("dev"); //这种方式，@Profile的效果还是失败
-        application.run(SpringbootstudyApplication.class, args);
+        final SpringApplication application = new SpringApplication(SpringbootstudyApplication.class);
+        final ConfigurableEnvironment env = application.run(args).getEnvironment();
+
+        LOG.info("启动成功！！");
+        LOG.info("地址: \thttp://127.0.0.1:{}", env.getProperty("server.port"));
 
         //查看容器里的所有组件
 //        String[] names = run.getBeanDefinitionNames();
