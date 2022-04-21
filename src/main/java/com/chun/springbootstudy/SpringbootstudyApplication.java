a8b5615288c2a5e6ce73b14154a91b387b4aaf34
chunxhwan <chunxhwan@126.com>
2022-04-21 10:49:21 +0800

    ## 对配置属性对象做校验判断（目前没做错误处理，出现错误程序直接停止）

--- src/main/java/com/chun/springbootstudy/SpringbootstudyApplication.java
+++ src/main/java/com/chun/springbootstudy/SpringbootstudyApplication.java
@@ -13,15 +13,15 @@ public class SpringbootstudyApplication {
         final ConfigurableApplicationContext run = SpringApplication.run(SpringbootstudyApplication.class, args);
 
         //查看容器里的所有组件
-        String[] names = run.getBeanDefinitionNames();
-        for (String name : names){
-            System.out.println(name);
-        }
-
-        Pet pet01 = run.getBean("德玛西亚", Pet.class);
-        Pet pet02 = run.getBean("德玛西亚", Pet.class);
-        System.out.println("是否相等：" + (pet01 == pet02));
-        System.out.println("是否相同：" + pet01.equals(pet02));
+//        String[] names = run.getBeanDefinitionNames();
+//        for (String name : names){
+//            System.out.println(name);
+//        }
+//
+//        Pet pet01 = run.getBean("德玛西亚", Pet.class);
+//        Pet pet02 = run.getBean("德玛西亚", Pet.class);
+//        System.out.println("是否相等：" + (pet01 == pet02));
+//        System.out.println("是否相同：" + pet01.equals(pet02));
     }
 
 }
--- src/main/java/com/chun/springbootstudy/config/MyChunProperties.java
+++ src/main/java/com/chun/springbootstudy/config/MyChunProperties.java
@@ -2,8 +2,10 @@ package com.chun.springbootstudy.config;
 
 import com.chun.springbootstudy.domain.MyPojo;
 import lombok.Data;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
 
 import java.net.InetAddress;
 import java.util.ArrayList;
@@ -14,7 +16,10 @@ import java.util.List;
 @Data
 public class MyChunProperties {
     private boolean enabled;
+
+    @NotNull
     private InetAddress remoteAddress;
+
     private final Security security = new Security();
 
     private final List<MyPojo> list = new ArrayList<>();
