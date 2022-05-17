147429dffa91030ef0f6014baedcd9e103a33579
chunxhwan <chunxhwan@126.com>
2022-05-17 14:59:07 +0800

    ## 组件注入到类的三种方式 1. 私有属性注入 2. 构造函数注入 3. setter注入
    注意：
    建议使用 构造函数 或 setter 注入，但是开发时都喜欢私有属性注入

---
+++ src/main/java/com/chun/springbootstudy/constroller/MoreTypeAutowiredConstroller.java
@@ -0,0 +1,26 @@
+package com.chun.springbootstudy.constroller;
+
+import com.chun.springbootstudy.domain.Diana;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/autowired")
+public class MoreTypeAutowiredConstroller {
+    // 第一种私有属性注入
+    // @Autowired
+    private Diana diana;
+
+    // 第二种构造函数注入
+    // @Autowired
+    public MoreTypeAutowiredConstroller(Diana diana){
+        this.diana = diana;
+    }
+
+    //第三种 setter注入
+    @Autowired
+    public void setDiana(Diana diana) {
+        this.diana = diana;
+    }
+}
