8c6145f412808d5e02b7c2fd0270f1024f363952
chunxhwan <chunxhwan@126.com>
2022-05-18 10:19:42 +0800

    ## @ConditionalOnProperty的使用 参考文档 https://blog.csdn.net/qq_42145871/article/details/118310230

--- src/main/java/com/chun/springbootstudy/config/MyConditionConfig.java
+++ src/main/java/com/chun/springbootstudy/config/MyConditionConfig.java
@@ -5,6 +5,7 @@ import com.chun.springbootstudy.condition.BananaCondition;
 import com.chun.springbootstudy.service.Fruit;
 import com.chun.springbootstudy.service.imp.Apple;
 import com.chun.springbootstudy.service.imp.Banana;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
@@ -12,13 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MyConditionConfig {
     @Bean
-    @Conditional(AppleCondition.class) //会根据条件，是否注入
+    // @Conditional(AppleCondition.class) //会根据条件，是否注入
+    @ConditionalOnProperty(value = "fruit.name", havingValue = "apple", matchIfMissing = true)
     public Fruit apple(){
         return new Apple();
     }
 
     @Bean
-    @Conditional(BananaCondition.class) //会根据条件，是否注入
+    // @Conditional(BananaCondition.class) //会根据条件，是否注入
+    @ConditionalOnProperty(value = "fruit.name", havingValue = "banana")
     public Fruit banana(){
         return new Banana();
     }
